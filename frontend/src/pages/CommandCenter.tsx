f57e21e5ec5bfccf5227590c06d5e11de3b21955
gustaquevedo-lab <gustaquevedo@gmail.com>
2026-05-02 23:06:48 -0300

    Fix CommandCenter crash by adding missing useMap import

--- frontend/src/pages/CommandCenter.tsx
+++ frontend/src/pages/CommandCenter.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, ZoomControl, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import {
